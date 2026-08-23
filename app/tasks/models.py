9858636d83afeaa9e0849ad5501a8c8e20756163
AminShah10 <aminshahmohamadi1384@gmail.com>
2026-08-23 17:01:23 +0330

    feat: model for database table created

---
+++ app/tasks/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, Column, String, Boolean, Text, DateTime, func
+from core.database import Base
+
+class TaskModel(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text(300), nullable=True)
+    is_done = Column(Boolean, default=False)
+    
+    create_date = Column(DateTime, server_default=func.now())
+    update_date = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
