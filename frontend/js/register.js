e279dd3054e43394487891b5ad1b6514d88bc3c5
AminShah10 <aminshahmohamadi1384@gmail.com>
2026-09-12 12:45:13 +0330

    feat: front-end added for login and registration

---
+++ frontend/js/register.js
@@ -0,0 +1,11 @@
+const registerForm = document.getElementById("register-form");
+
+registerForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log("Username:", username);
+    console.log("Password:", password);
+});
